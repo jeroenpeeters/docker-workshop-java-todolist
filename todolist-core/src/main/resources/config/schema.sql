af8f18b9d659ef0d9f06cead5e23572fce97c501
jeroenpeeters <jeroen@peetersweb.nl>
2016-10-23 17:16:41 +0200

    disabled sql schema, db is autocreate

--- todolist-core/src/main/resources/config/schema.sql
+++ todolist-core/src/main/resources/config/schema.sql
@@ -1,18 +1,18 @@
-CREATE TABLE "user" (
-  id int NOT NULL primary key,
-  name varchar(32) DEFAULT NULL,
-  email varchar(32) NOT NULL,
-  password varchar(32) DEFAULT NULL
-);
-
-
-CREATE TABLE "todo" (
-  id int NOT NULL primary key,
-  userId int NOT NULL references "user"(id),
-  title varchar(512) DEFAULT NULL,
-  done boolean DEFAULT FALSE NOT NULL,
-  priority int NOT NULL,
-  dueDate date DEFAULT NULL
-);
-
-GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+-- CREATE TABLE IF NOT EXISTS "user" (
+--   id int NOT NULL primary key,
+--   name varchar(32) DEFAULT NULL,
+--   email varchar(32) NOT NULL,
+--   password varchar(32) DEFAULT NULL
+-- );
+--
+--
+-- CREATE TABLE IF NOT EXISTS "todo" (
+--   id int NOT NULL primary key,
+--   userId int NOT NULL references "user"(id),
+--   title varchar(512) DEFAULT NULL,
+--   done boolean DEFAULT FALSE NOT NULL,
+--   priority int NOT NULL,
+--   dueDate date DEFAULT NULL
+-- );
+--
+-- GRANT ALL PRIVILEGES ON DATABASE todo TO postgres;
